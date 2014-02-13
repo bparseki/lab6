7ab2be847ecea1e1d688a72a42278d34773c4442
bparseki <bparseki@ucsd.edu>
2014-02-13 22:07:04 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,7 +26,19 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	$.get("/project/" + idNumber, addProject);
 	console.log("User clicked on project " + idNumber);
+}
+
+function addProject(result) {
+	console.log(result);
+	var id = result['id']; 
+	var projectDetails = "#project" + id + " .details";
+	var projectHTML = '<a href="#">' +
+		'<img  scr="' + result['image'] + '" class = detailsImage">' +
+		'<p><small>' + result['date'] + '<small></p>' +
+		'<p>' + result['summary'] + '</p></a>';
+	$(projectDetails).html(projectHTML);
 }
 
 /*
@@ -35,4 +47,18 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	// var ramdomColor = $(this).closest('.project').attr('id');
+	$.get("/palette/", addColor);
+	console.log("User clicked on color");
+}
+
+function addColor(result) {
+	console.log(result);
+	var colors = result['colors']['hex'];
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
